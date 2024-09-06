53d050e673337e130d51ec2ccabd0c9059fbbcc8
Bailey Nguyen <bailey.tan.nguyen@gmail.com>
2024-09-06 00:12:00 -0500

    Some playing

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Successful response.');
-});
+app.use('/', express.static(path.join(__dirname, 'public')));
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'));
+//   });  
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
